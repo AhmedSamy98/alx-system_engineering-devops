f7f66ca3d80304560391f3a90682c3167d4704fc
AhmedSamy98 <ahmed1samy1998@gmail.com>
2023-12-22 18:54:17 -0800

    Fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# This script uses pip to install flask
-exec { 'flask':
-  command => '/usr/bin/pip install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip show flask',
+# this Puppet manifest installs the puppet-lint package
+package {'Flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
