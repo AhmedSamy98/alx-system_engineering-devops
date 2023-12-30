8a0d924e550c6bad28f5ae0d95cf76938c3f8c6e
AhmedSamy98 <ahmed1samy1998@gmail.com>
2023-12-30 10:46:34 -0800

    FIX {1. Install a package}

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
-# Installs flask python package using pip3 installer
+# Install specific version of Werkzeug
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+# Install Flask with version 2.1.0
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
