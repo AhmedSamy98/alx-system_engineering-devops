491eb8f58a3648d6c37c8eea3aa5536a3ff38811
AhmedSamy98 <ahmed1samy1998@gmail.com>
2024-01-30 13:35:40 -0800

    DELETE

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-request.get(process.argv[2], function (error, response, body) {
-  if (error) throw error;
-  else console.log('code:', response.statusCode);
-});
