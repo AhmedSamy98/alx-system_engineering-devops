5c5be71b2893948450b36a6c158db61e713c91d7
AhmedSamy98 <ahmed1samy1998@gmail.com>
2024-01-29 16:54:55 -0800

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
-"""
-    This module gets an info on an employee using REST API
-"""
+'''
+Python script that returns information using REST API
+'''
 import requests
-import sys
-
+from sys import argv
 
 if __name__ == "__main__":
-    """ if module is run directly DO """
-    user_id = sys.argv[1]
-    user_todo = requests.get(f'https://jsonplaceholder.typicode.com/users/'
-                             f'{user_id}/todos')
-    user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/'
-                             f'{user_id}')
-
-    if user_todo.status_code == 200:
-        if user_info.status_code == 200:
-            user_todo = user_todo.json()
-            user_info = user_info.json()
-
-            user_name = user_info.get("name")
-            total_tasks = len(user_todo)
-            done_tasks = 0
-            titles = []
-
-            for i in range(len(user_todo)):
-                if user_todo[i].get("completed") is True:
-                    done_tasks += 1
-                    titles.append(user_todo[i].get("title"))
-            print(f"Employee {user_name} is done with "
-                  f"tasks({done_tasks}/{total_tasks}):")
-            for title in titles:
-                print(f"\t {title}")
-        else:
-            print(f"Error: Unable to fetch data user info"
-                  f"(Status code {user_info.status_code})")
-    else:
-        print(f"Error: Unable to fetch data user todo"
-              f"(Status code {user_todo.status_code})")
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
